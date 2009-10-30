5cfe9dd19450b0f99275042094ea405417e1b36c
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2009-10-30 22:32:30 +0000

    [fsmi_vkrit] * CSV file import and check implemented * missing: SQL queries for insertion
    git-svn-id: https://svn-serv.cs.upb.de/fsmi-dev/trunk/fsmi_vkrit@76 2b0ac4d0-5be0-11de-a9ea-3b1067c9442c

---
+++ api/class.tx_fsmivkrit_div.php
@@ -0,0 +1,66 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Andreas Cord-Landwehr (cola@uni-paderborn.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+* This class provides a huge amount on utility functions, e.g. for database access...
+*
+* @author Andreas Cord-Landwehr <cola@uni-paderborn.de>
+*/
+
+
+
+require_once(PATH_t3lib.'class.t3lib_befunc.php');
+require_once(PATH_t3lib.'class.t3lib_tcemain.php');
+require_once(PATH_t3lib.'class.t3lib_iconworks.php');
+
+/**
+ * Script Class to download files as defined in reports
+ *
+ */
+class tx_fsmivkrit_div {
+	const kSTATUS_INFO = 0;
+	const kSTATUS_ERROR = 1;
+
+	/**
+	 *
+	 * @param integer $status from constants
+	 * @param string $text information text
+	 * @return string of HTML div box
+	 */
+	function printSystemMessage($status, $text) {
+		$content = '';
+		$content .= '<div style="padding: 5px; border: 2px dotted; background-color: #eee; margin-top: 10px; max-width: 400px;">';
+		// TODO switch $status
+		$content .= $text;
+		$content .= '</div>';
+		
+		return $content;
+	}
+
+}
+
+// Include extension?
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fsmi_vkrit/api/class.tx_fsmivkrit_div.php'])    {
+    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fsmi_vkrit/api/class.tx_fsmivkrit_div.php']);
+}
+?>
--- pi4/class.tx_fsmivkrit_pi4.php
+++ pi4/class.tx_fsmivkrit_pi4.php
@@ -28,7 +28,7 @@
  */
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
-
+require_once(t3lib_extMgm::extPath('fsmi_vkrit').'api/class.tx_fsmivkrit_div.php');
 
 /**
  * Plugin 'Data Import/Export' for the 'fsmi_vkrit' extension.
@@ -38,10 +38,29 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  * @subpackage	tx_fsmivkrit
  */
 class tx_fsmivkrit_pi4 extends tslib_pibase {
-	var $prefixId      = 'tx_fsmivkrit_pi4';		// Same as class name
-	var $scriptRelPath = 'pi4/class.tx_fsmivkrit_pi4.php';	// Path to this script relative to the extension dir.
-	var $extKey        = 'fsmi_vkrit';	// The extension key.
+	var $prefixId      	= 'tx_fsmivkrit_pi4';		// Same as class name
+	var $scriptRelPath 	= 'pi4/class.tx_fsmivkrit_pi4.php';	// Path to this script relative to the extension dir.
+	var $extKey        	= 'fsmi_vkrit';	// The extension key.
 	
+	// types
+	var $kIMPORT		= 1;
+	var $kEXPORT		= 2;
+	
+	// csv Columns
+	var $kCSV_FUNKTION 		= 0;
+	var $kCSV_ANREDE 		= 1;
+	var $kCSV_TITEL			= 2;
+	var $kCSV_VORNAME		= 3;
+	var $kCSV_NACHNAME		= 4;
+	var $kCSV_EMAIL			= 5;
+	var $kCSV_LV_NAME		= 6;
+	var $kCSV_LV_KENNUNG	= 7;
+	var $kCSV_LV_ORT		= 8;
+	var $kCSV_STUDIENGANG	= 9;
+	var $kCSV_LV_ART		= 10;
+	var $kCSV_TEILNEHMER	= 11;
+	var $kCSV_ORGAEINHEIT	= 12;
+
 	/**
 	 * The main method of the PlugIn
 	 *
@@ -55,23 +74,185 @@ class tx_fsmivkrit_pi4 extends tslib_pibase {
 		$this->pi_loadLL();
 		$this->pi_USER_INT_obj = 1;	// Configuring so caching is not expected. This value means that no cHash params are ever set. We do this, because it's a USER_INT object!
 	
-		$content='
-			<strong>This is a few paragraphs:</strong><br />
-			<p>This is line 1</p>
-			<p>This is line 2</p>
-	
-			<h3>This is a form:</h3>
-			<form action="'.$this->pi_getPageLink($GLOBALS['TSFE']->id).'" method="POST">
-				<input type="text" name="'.$this->prefixId.'[input_field]" value="'.htmlspecialchars($this->piVars['input_field']).'">
-				<input type="submit" name="'.$this->prefixId.'[submit_button]" value="'.htmlspecialchars($this->pi_getLL('submit_button_label')).'">
-			</form>
-			<br />
-			<p>You can click here to '.$this->pi_linkToPage('get to this page again',$GLOBALS['TSFE']->id).'</p>
-		';
-	
+		$content = '';
+		
+		
+		// type selection head
+		$content .= $this->createTypeSelector();
+		
+		// select input type
+		$GETcommands = t3lib_div::_GP($this->extKey);	// can be both: POST or GET
+		switch (intval($GETcommands['type'])) {
+			case $this->kIMPORT: {
+				// check for POST data
+				if (t3lib_div::_POST($this->extKey)) {
+					// form files
+					$formDataFiles = $_FILES[$this->extKey];
+					
+					// get files
+					if ($GETcommands['file_confirmed']) {
+						$csvArray = $this->loadImportData(htmlspecialchars($GETcommands['file_confirmed']));
+						t3lib_div::unlink_tempfile($GETcommands['file_confirmed']);
+					} else {
+						$filepath = t3lib_div::upload_to_tempfile($formDataFiles['tmp_name']['file']);		
+						$csvArray = $this->loadImportData($filepath);
+					}
+						
+					// set status message
+					if (count($csvArray)==0)
+						$content .= tx_fsmivkrit_div::printSystemMessage(
+								tx_fsmivkrit_div::kSTATUS_ERROR, 
+								'Leere Datei/konnte Datei nicht lesen');
+					else {
+						// is there a confirmed filed
+						if ($GETcommands['file_confirmed']) {
+							$this->saveImportData($csvArray, intval($GETcommands['storage']));
+							$content .= tx_fsmivkrit_div::printSystemMessage(
+									tx_fsmivkrit_div::kSTATUS_INFO, 
+									'Daten gespeichert.');
+							$content .= $this->printImportData($csvArray);
+						} else {
+							$content .= tx_fsmivkrit_div::printSystemMessage(
+									tx_fsmivkrit_div::kSTATUS_INFO, 
+									'Datei Eingelesen, aber noch nicht gespeichert!');
+							$content .= $this->createImportDataConfirmForm($filepath);
+							$content .= $this->printImportData($csvArray);
+						}
+					}
+				}
+				else
+					$content .= $this->createImportDataForm();
+				break;
+			}
+			default: 
+				break;
+		}
+		
 		return $this->pi_wrapInBaseClass($content);
 	}
+	
+	function createTypeSelector () {
+		$content = '<div>';
+		$content .= $this->pi_linkTP('Import PAUL Data', 
+								array (	$this->extKey.'[type]' => $this->kIMPORT));
+		$content .= ' | ';
+		$content .= $this->pi_linkTP('Export EvaSys Data', 
+								array (	$this->extKey.'[type]' => $this->kEXPORT));
+		$content .= '</div>';
+		
+		return $content;
+								
+	}
+	
+	function createImportDataForm() {
+		$content = '';
+		$content .= '<form action="'.$this->pi_getPageLink($GLOBALS["TSFE"]->id).'" method="POST" enctype="multipart/form-data" name="'.$this->extKey.'">';
+		
+		// hidden field to tell system, that IMPORT data is coming
+		$content .= '<input type="hidden" name="'.$this->extKey.'[type]'.'" value='.$this->kIMPORT.' />';
+		
+		$content .= '<fieldset>
+			<label for="'.$this->extKey.'_file">Importdatei:</label>
+			<input type="file" name="'.$this->extKey.'[file]" id="'.$this->extKey.'_file"  	
+					value="'.htmlspecialchars($this->piVars["file"]).'" />
+			<div>
+				Erwartet wird eine CSV Datei mit EXAKT den spezifizierten Werten. 
+				Vor einer endgültigen Speicherung werden die zu speichernden Daten angezeigt.<br />
+				<pre>Funktion,Anrede,Titel,Vorname,Nachname,Email,LV-Name,LV-Kennung,LV-Ort,Studiengang,LV-Art,Teilnehmer,Orgaeinheit</pre>
+				Aufgrund desolater PAUL-Daten muss jede VL exakt einmal in der Liste stehen. Die erste Zeile wird ignoriert.				
+			</div>
+			</fieldset>
+			
+			<fieldset>
+				<label for="'.$this->extKey.'_import_survey">Umfrage:</label>
+				<select name="'.$this->extKey.'[survey]" id="'.$this->extKey.'_survey"  	
+					value="'.htmlspecialchars($this->piVars["survey"]).'">
+					';
+		
+		$res = $GLOBALS['TYPO3_DB']->sql_query('SELECT * 
+												FROM tx_fsmivkrit_survey 
+												WHERE deleted=0 AND hidden=0');
+		
+		while ($res && $row = mysql_fetch_assoc($res))
+			$content .= '<option value="'.$row['uid'].'">'.$row['semester'].' - '.$row['name'].'</option>';
+		
+		$content .= '</select>';
+		$content .= '<div>Die entsprechende Umfrage muss über das Backend bereits angelegt worden sein.</div>
+			</fieldset>';
+		
+		$content .= '<input type="submit" name="'.$this->extKey.'[submit_button]" 
+				value="'.htmlspecialchars('Datei überprüfen').'">';
+		$content .= '</form>';
+		
+		return $content;	
+	}
+	
+	function createImportDataConfirmForm ($filepath) {
+		$content = '';
+		$content .= '<form action="'.$this->pi_getPageLink($GLOBALS["TSFE"]->id).'" method="POST" enctype="multipart/form-data" name="'.$this->extKey.'">';
+		
+		// hidden field to tell system, that IMPORT data is coming
+		$content .= '<input type="hidden" name="'.$this->extKey.'[type]'.'" value='.$this->kIMPORT.' />';
+		$content .= '<input type="hidden" name="'.$this->extKey.'[file_confirmed]'.'" value="'.$filepath.'" />';
+		$content .= '<input type="submit" name="'.$this->extKey.'[submit_button]" 
+				value="'.htmlspecialchars('Import abschließen').'">';
+		
+		$content .= '</form>';
+		
+		return $content;
+	}
+	
+	/**
+	 * Function reads CSV File from $_FILES[fsmi_vkrit][file] and
+	 * imports everything as array
+	 * 
+	 * @return array $csvArray
+	 */
+	function loadImportData ($filepath) {		
+		if ($filepath=='')
+			return array ();
+			
+		$csvArray = array ();	
+		$file = fopen($filepath, 'r');
+		while (($data = fgetcsv($file)))
+    		array_push($csvArray, $data);
+    	fclose($file);
+		
+		return $csvArray;
+	}
+	
+	function printImportData ($csvArray) {
+		$content = '<table>';
+		for ($i=0; $i<count($csvArray); $i++)
+			$content .= '<tr>'.
+				'<td>'.$csvArray[$i][$this->kCSV_NACHNAME].'</td>'.
+				'<td>'.$csvArray[$i][$this->kCSV_EMAIL].'</td>'.
+				'<td>'.$csvArray[$i][$this->kCSV_LV_NAME].'</td>'.
+				'</tr>';
+		$content .= '</table>';
+				
+		return $content;
+	}
+	
+	function saveImportData ($csvArray, $storage) {
+		// dismiss first line
+//TODO until now everything prepared to create lecturers and lectures
+//		for ($i=1; $i<count($csvArray); $i++) {
+//		
+//			$res = $GLOBALS['TYPO3_DB']->exec_INSERTquery(	
+//									'tx_fsmivkrit_lecture',
+//									array (	'pid' => $this->storageLecturer,
+//											'crdate' => time(),
+//											'tstamp' => time(),
+//											'firstname' => $GLOBALS['TYPO3_DB']->quoteStr($formData['firstname'], 'tx_fsmiexams_lecturer'),
+//											'lastname' => $GLOBALS['TYPO3_DB']->quoteStr($formData['lastname'], 'tx_fsmiexams_lecturer'),
+//									));
+//		//TODO check if $res exists 
+//		}
+	}
+	
 }
+
 
 
 
